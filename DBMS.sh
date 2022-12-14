3158ec30af7de15c639b6610fac1244b47cd022a
Alii2121 <alimarawan2121@hotmail.com>
2022-12-14 21:06:55 +0200

    Added list + Drop + Connect options + some visual changes

--- DBMS.sh
+++ DBMS.sh
@@ -1,30 +1,95 @@
 #!usr/bin/bash
 
-mkdir DBMS 2> ~/ERORRS
+mkdir ./DBMS 2>/dev/null
+
 
   ###START of script and intro choices###
 
-echo -e "-------------- Hello Ali's DBMS User :) ------------
- 
+echo "
 
-            What are you up to Today?"
+--------------- Hello Ali's DBMS User :) ------------
+ 
+            What are you up to Today?
+                                                    
+                                                    "
 
 echo  "1) Create Database 
 2) List Databases
 3) Connect to a Database
 4) Drop Database "
+echo "
 
-read ch1 
+--------------------------------------------------- "
+echo -n  "Ask and you shall be served :) "
+read upto 
 
+case $upto in
 
+ 1 )
 
-if [[ $ch1 = 1 ]]; then
-      
-      read -p "  Please Enter The New Database Name : " cdb 
+    read -p "  Please Enter The New Database Name : " cdb 
         if [ -d "./DBMS/$cdb" ]; then
-            echo " Database Already exist !"
+            echo " Database Already exists !"
         else
              mkdir ./DBMS/$cdb
              echo " Database Created Successfully ! "
         fi
-fi                  
+;;
+
+
+2 )
+    
+    if [ -d "./DBMS" ]; then
+         ls ./DBMS/ 
+    else 
+         echo " Encountered an error"
+    fi          
+
+;;
+
+3 ) 
+    
+    read -p "Please Enter DB Name : " selectdb
+    
+    if [ -d "./DBMS/$selectdb" ]; then
+    
+        cd ./DBMS/$selectdb
+        echo "Connected to Database Successfully !"
+    else 
+         echo " No Database with that name exists !"
+    fi     
+
+;;
+
+
+4 ) 
+
+    read -p "Enter the DB Name that you wish to DROP : " dropdb
+
+    if [ -d "./DBMS/$dropdb" ]; then
+        
+        rm -r ./DBMS/$dropdb
+        echo " DB Dropped Successfully ! "
+
+    else
+
+          echo " DB Doesn't Exist "    
+    fi
+;;
+
+
+
+* )
+
+   echo "bye"
+   
+esac
+
+
+
+
+
+
+
+
+
