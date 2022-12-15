cb7d7c587b4273e3c47d4ec1b0dc4c4fd6452547
Alii2121 <alimarawan2121@hotmail.com>
2022-12-15 16:07:58 +0200

    solved regex problem for table creation to make col. no. accept only integers

--- createTable.sh
+++ createTable.sh
@@ -9,11 +9,24 @@ read tablename
      echo -e "\n Table already exists ! \n \c  "
      
   elif 
-         [[ ! $tablename =~  ^[a-zA-Z]+[a-zA-Z0-9]*$ ]] || [[ $tablename == '' ]]; then
+         [[ ! $tablename =~ ^[a-zA-Z]+[a-zA-Z0-9]*$ ]] || [[ $tablename == '' ]]; then
          echo -e "\n Table Name Can't Start with a space or Special Characters \n "
        
+                  
+
   else 
-          
-          touch ./$tablename
-          echo -e "\n Table Created Successfully"
+        
+          echo -e "\n Please Enter No. of Coloumns: \n \c"
+
+         read nocols 
+       if [[ $nocols == 0 ]]; then
+              echo " no. of coloumns can't be 0 "
+       elif
+            ! [[ "$nocols" =~ ^[0-9]+$ ]]; then
+                echo -e '\n Please Enter Numbers Only !! \n'
+       else           
+           touch ./$tablename
+           touch ./$tablename"-meta_data"
+           echo -e "\n Table Created Successfully"
+       fi    
   fi
